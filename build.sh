2148adc87108961cb8b7cb077ae35d9922e2120f
idchlife <idchlife@gmail.com>
2019-05-05 10:35:33 +0300

    onefile in build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 echo "Building..."
-pipenv run pyinstaller cli.py -p $(pipenv --venv)"/lib/python3.7/site-packages" --add-data "app/web/templates:app/web/templates" --add-data "app/web/assets:app/web/assets"
+pipenv run pyinstaller --onefile cli.py -p $(pipenv --venv)"/lib/python3.7/site-packages" --add-data "app/web/templates:app/web/templates" --add-data "app/web/assets:app/web/assets"
